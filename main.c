0d907d6898b06398d383edf6f7597689285a499f
skeps <aliskepss@gmail.com>
2026-05-28 01:45:39 +0100

    - add "add task" function - change task count to 99, max tasks is 100, so we leave one last char for \0 exit

--- main.c
+++ main.c
@@ -12,9 +12,19 @@ Task tasks[MAX_TASKS];
 int task_count = 0;
 
 void add_task(char description[]){
-    strncpy(tasks[task_count].description, description, 100);
+    if (task_count >= MAX_TASKS) {
+        printf("Task limit reached!\n");
+        return;
+    }
+    strncpy(tasks[task_count].description, description, 99);
+    tasks[task_count].description[99] = '\0';
     tasks[task_count].done = 0;
     task_count++;
+}
+void list_tasks(){
+    for (int i = 0; i < task_count; i++) {
+        printf("Task %d: %s\n", i + 1, tasks[i].description);
+    }
 }
 
 
@@ -22,6 +32,7 @@ int main(int argc, char *argv[]){
     printf("Task Tracker\n");
     add_task("Buy groceries");
     add_task("Finish project");
+    list_tasks();
     printf("Total tasks: %d\n", task_count);
     return 0;
 }
